3ecc8036308202f310c4864d2644fe55196a9cfe
Matias Ramirez <matiasgr3009@gmail.com>
2026-04-12 12:27:07 -0300

    solved type issue

--- types/product.ts
+++ types/product.ts
@@ -3,6 +3,7 @@ export interface Product {
     name: string;
     price: number;
     regularPrice: number;
+    regularListPrice: number;
     onSale: boolean;
     image: string;
     images: string[];
