8f3ba1744e52e6dce9666f2d759f18c672276e97
Matias Ramirez <matiasgr3009@gmail.com>
2026-05-21 08:49:59 -0300

    solving issues

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,9 @@ const nextConfig: NextConfig = {
         ],
     },
     serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+    outputFileTracingIncludes: {
+        "/*": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+    },
 };
 
 export default nextConfig;
