94f251552a618efb67b2392dca08c56359773e30
Matias Ramirez <matiasgr3009@gmail.com>
2026-06-08 09:02:18 -0300

    solving serach bar issues

--- services/products/searchProducts.ts
+++ services/products/searchProducts.ts
@@ -34,7 +34,7 @@ export async function searchProducts(search: string) {
                 slug
                 regularPrice
                 salePrice
-                images
+                image
                 availableStock
                 sku
                 `,
