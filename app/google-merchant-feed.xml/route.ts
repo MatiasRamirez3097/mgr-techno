ba3fce43c6bf124cb2ba71883c88334c13fc951a
Matias Ramirez <matiasgr3009@gmail.com>
2026-07-22 15:12:28 -0300

    adding meta events

--- app/google-merchant-feed.xml/route.ts
+++ app/google-merchant-feed.xml/route.ts
@@ -36,7 +36,7 @@ export async function GET() {
     <g:availability>
         ${product.isAvailable ? "in_stock" : "out_of_stock"}
     </g:availability>
-
+    <g:quantity_to_sell_on_facebook>${product.availableStock}</g:quantity_to_sell_on_facebook>
     <g:price>${price} ARS</g:price>
 
     ${
