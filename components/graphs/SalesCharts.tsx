15f7071273afea4e73439e9733621893eee32756
Matias Ramirez <matiasgr3009@gmail.com>
2026-07-29 10:32:26 -0300

    adding dashboard removed white bg

--- components/graphs/SalesCharts.tsx
+++ components/graphs/SalesCharts.tsx
@@ -27,7 +27,7 @@ export default function SalesCharts({ data }: { data: any[] }) {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
             {/* GRÁFICO 1: Ingresos por Mes (Barras) */}
             <div className="p-6 rounded-xl shadow-sm border border-gray-100">
-                <h3 className="text-lg font-bold text-gray-800 mb-4">
+                <h3 className="text-lg font-bold text-gray-100 mb-4">
                     Ingresos Mensuales
                 </h3>
                 <div className="h-72">
@@ -65,7 +65,7 @@ export default function SalesCharts({ data }: { data: any[] }) {
 
             {/* GRÁFICO 2: Cantidad de Ventas (Líneas) */}
             <div className="p-6 rounded-xl shadow-sm border border-gray-100">
-                <h3 className="text-lg font-bold text-gray-800 mb-4">
+                <h3 className="text-lg font-bold text-gray-100 mb-4">
                     Ventas Concretadas
                 </h3>
                 <div className="h-72">
